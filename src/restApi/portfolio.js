25aabb1950d64674e0f055e3465e40440faa5867
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-09-20 14:21:58 +0530

    updated the deploy link

--- src/restApi/portfolio.js
+++ src/restApi/portfolio.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://portfolio-data-server.onrender.com",
+  baseURL: "https://portfolio-data-server.vercel.app/",
+  // baseURL: "https://portfolio-data-server.onrender.com",
 });
